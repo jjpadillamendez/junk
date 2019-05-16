70265fce3f2381953bb39571ec2427eae3e91b19
jjpadillamendez <jjpadillamendez@outlook.com>
2019-05-16 17:29:11 -0600

    Changed output message to Club America

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Junk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Club America");
             Console.Read();
         }
     }
